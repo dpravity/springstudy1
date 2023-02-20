2d536dcdee7f75225f988167cd23e9931d84e334
DESKTOP-KJG7BHJ\최혜령 <dpravity@gmail.com>
2023-02-21 01:00:13 +0900

    spring container init 시 dispatcher servlet 초기화 등록

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -22,20 +22,27 @@ public class SpringbootApplication {
         // 스프링 컨테이너 생성
         // ApplicationContext : 애플리케이션을 구성하는 정보를 담고 있음, Bean, Resources 등등
         // ApplicationContext 가 결국 스프링 컨테이너가 된다
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+            // 스프링 컨테이너 초기화
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                // 컨테이너 초기화 중 dispatcher servlet 등록
+                ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*"); // 서블릿으로 들어오는 모든 요청 처리를 위해 와일드 카드
+                });
+                // tomcat servlet container 동작
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
+        // 스프링 컨테이너 초기화, 템플릿 메소드 패턴(상속을 통해 기능 확장)
         applicationContext.refresh();
-
-        ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(applicationContext)
-                    ).addMapping("/*"); // 서블릿으로 들어오는 모든 요청 처리를 위해 와일드 카드
-        });
-
-        // tomcat servlet container 동작
-        webServer.start();
     }
 
 }
