aed3e84302c7e797a7e57bee59efe9ed9f97830e
DESKTOP-KJG7BHJ\최혜령 <dpravity@gmail.com>
2023-02-21 00:54:49 +0900

    Dispatcher servlet add Controller annotation

--- src/main/java/com/example/springboot/contorller/HelloController.java
+++ src/main/java/com/example/springboot/contorller/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.springboot.contorller;
 
-import com.example.springboot.entity.Hello;
 import com.example.springboot.service.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+/**
+ * Dispatcher servlet 읽기 순서
+ * 1. 클래스 레벨
+ * @ReestController
+ * 2. 메소드 레벨
+ * @GetMapping
+ */
 @RestController
 public class HelloController {
 
@@ -22,15 +26,6 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name));
-//        return "hello : " + name;
     }
 
-    @PostMapping("/hellopost2")
-    public String hellopost(@RequestBody String name){
-        return "hello : " + name;
-    }
-    @PostMapping("/hellopost")
-    public String hellopost(@RequestBody Hello hello){
-        return "hello : " + hello.getName();
-    }
 }
